bdce0e487d08d783844d1b0c0c521e433c1e871c
Robert Loehr <robert.loehr@colostate.edu>
2026-04-27 09:29:44 -0600

    input checks

--- solution.py
+++ solution.py
@@ -4,4 +4,7 @@ from two_opt import two_opt
 
 
 def ga_tsp(initial_population, distances, generations):
-    pass
+    if initial_population is None or distances is None or generations is None:
+        raise ValueError("Invalid argument")
+    if generations <= 0:
+        raise ValueError("Invalid argument")
